1ad4d526472b50aa52fed9df06585230bc1e0162
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-10-27 21:27:28 +0200

    Fetch url and store with pae title in localStorage

--- bookmarker/app/renderer.js
+++ bookmarker/app/renderer.js
@@ -1,3 +1,5 @@
+const parser = new DOMParser();
+
 const linksSection = document.querySelector('.links');
 const errorMessage = document.querySelector('.error-message');
 const newLinkForm = document.querySelector('.new-link-form');
@@ -5,10 +7,28 @@ const newLinkUrl = document.querySelector('.new-link-url');
 const newLinkSubmit = document.querySelector('.new-link-submit');
 const clearStorageButton = document.querySelector('.clear-storage');
 
+const parseResponse = text => parser.parseFromString(text, 'text/html');
+const findTitle = nodes => nodes.querySelector('title').innerText;
+const clearForm = () => {
+  newLinkUrl.value = null;
+};
+const storeLink = url => title => {
+  localStorage.setItem(url, JSON.stringify({ title, url }));
+};
+
 newLinkUrl.addEventListener('keyup', () => {
   newLinkSubmit.disabled = !newLinkUrl.validity.valid;
 });
 
-const clearForm = () => {
-  newLinkUrl.value = null;
-};
+newLinkForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const url = newLinkUrl.value;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(parseResponse)
+    .then(findTitle)
+    .then(storeLink(url))
+    .then(clearForm);
+});
