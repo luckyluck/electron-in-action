854fec752195b515e3d8bfde49faac307b697a75
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-10-29 22:42:17 +0200

    Set window title and set document edited

--- firesale/app/main.js
+++ firesale/app/main.js
@@ -12,6 +12,11 @@ app.on('ready', () => {
   ipcMain.on('new-file', () => {
     createWindow();
   });
+
+  ipcMain.on('file-update', (_, title, isEdited) => {
+    BrowserWindow.getFocusedWindow().setTitle(title);
+    BrowserWindow.getFocusedWindow().setDocumentEdited(isEdited);
+  });
 });
 
 app.on('activate', (_, hasVisibleWindows) => {
--- firesale/app/renderer.js
+++ firesale/app/renderer.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require('electron');
 const marked = require('marked');
 const createDOMPurify = require('dompurify');
+const path = require('path');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -14,12 +15,32 @@ const openInDefaultButton = document.querySelector('#open-in-default');
 
 const DOMPurity = createDOMPurify();
 
+let filePath;
+let originalContent = '';
+
 const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitizer: DOMPurity.sanitize });
 };
 
+const updateUserInterface = (isEdited = false) => {
+  let title = 'Fire Sale';
+
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+  if (isEdited) {
+    title = `${title} (Edited)`;
+  }
+
+  ipcRenderer.send('file-update', title, isEdited);
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+};
+
 markdownView.addEventListener('keyup', e => {
   renderMarkdownToHtml(e.target.value);
+  updateUserInterface(e.target.value !== originalContent);
 });
 
 newFileButton.addEventListener('click', () => {
@@ -28,7 +49,12 @@ newFileButton.addEventListener('click', () => {
 
 openFileButton.addEventListener('click', () => {
   ipcRenderer.invoke('open-file').then(({ file, content }) => {
+    filePath = file;
+    originalContent = content;
+
     markdownView.value = content;
     renderMarkdownToHtml(content);
+
+    updateUserInterface();
   });
 });
