940ca2c372c82e46ff2f61b46b18e2c4fdc16228
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-11-03 21:26:06 +0100

    Supporting dark mode in macOS

--- clipmaster/app/utils.js
+++ clipmaster/app/utils.js
@@ -1,6 +1,12 @@
+const { nativeTheme } = require('electron');
+
 const getIcon = () => {
   if (process.platform === 'wind32') {
     return 'icon-light@2x.ico';
+  }
+
+  if (nativeTheme.shouldUseDarkColors) {
+    return 'icon-light.png';
   }
 
   return 'icon-dark.png';
