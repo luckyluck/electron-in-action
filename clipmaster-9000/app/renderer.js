75cc730e062980cb7255089b30d393ee965deba0
Alexandr Ketov <alexandr.ketov@doktor24.se>
2021-11-04 22:26:36 +0100

    Working with clippings in the application

--- clipmaster-9000/app/renderer.js
+++ clipmaster-9000/app/renderer.js
@@ -1,4 +1,9 @@
 const { clipboard } = require('electron');
+const request = require('request').defaults({
+  url: 'https://cliphub.glitch.me/clippings',
+  headers: { 'User-Agent': 'Clipmaster 9000' },
+  json: true,
+});
 
 const clippingsList = document.getElementById('clippings-list');
 const copyFromClipboardButton = document.getElementById('copy-from-clipboard');
@@ -26,4 +31,33 @@ const addClippingToList = () => {
   clippingsList.prepend(clippingElement);
 };
 
+const getButtonParent = ({ target }) => target.parentNode.parentNode;
+
+const removeClipping = target => target.remove();
+
+const getClippingText = clippingListItem =>
+  clippingListItem.querySelector('.clipping-text').innerText;
+
+const writeToClipboard = text => clipboard.writeText(text);
+
+const publishingClipping = clipping => {
+  request.post({ json: { clipping } }, (error, response, body) => {
+    if (error) {
+      return alert(JSON.parse(error).message);
+    }
+
+    const url = body.url;
+    alert(url);
+    clipboard.writeText(url);
+  });
+};
+
 copyFromClipboardButton.addEventListener('click', addClippingToList);
+
+clippingsList.addEventListener('click', e => {
+  const hasClass = className => e.target.classList.contains(className);
+
+  if (hasClass('remove-clipping')) removeClipping(getButtonParent(e));
+  if (hasClass('copy-clipping')) writeToClipboard(getClippingText(getButtonParent(e)));
+  if (hasClass('publish-clipping')) publishingClipping(getClippingText(getButtonParent(e)));
+});
